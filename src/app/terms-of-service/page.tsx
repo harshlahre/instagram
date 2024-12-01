ca59570124df5f1d6bb7c2edd799f5d3fc3d1589
harshlahre <harshlahre109@gmail.com>
2024-12-01 22:43:02 +0530

    this

--- src/app/terms-of-service/page.tsx
+++ src/app/terms-of-service/page.tsx
@@ -2,139 +2,98 @@ const TermsOfServicePage = () => {
   return (
     <div className="my-4 flex flex-col rounded-lg border p-8">
       <h1 className="mb-4 text-2xl font-bold">Terms of Service</h1>
-      <section className="mb-2 text-sm">
-        <h2 className="mb-2 text-xl font-semibold">1. Introduction</h2>
-        <p className="mb-2">
-          <span className="font-semibold">1.1.</span> This Terms of Service
-          (ToS) governs your use of the website
-          <span className="text-gray-900 dark:text-gray-500">
-            {" "}
-            (insta-video-saver.vercel.app).
-          </span>{" "}
-          By using the Website or Service, you agree to be bound by the terms
-          and conditions set forth in this ToS.
-        </p>
-        <p className="mb-2">
-          <span className="font-semibold">1.2.</span> Please read this ToS
-          carefully before using the Website or Service. If you do not agree to
-          the terms of this ToS, you must not access or use the Website or
-          Service.
-        </p>
-      </section>
+      <section className="terms-privacy">
+<p>Welcome to Insta Load! These terms and conditions outline the rules and regulations for the use of Insta Load's Website, located at <a href="https://instaload.hindisink.com">https://instaload.hindisink.com</a>.</p>
 
-      <section className="mb-2 text-sm">
-        <h2 className="mb-2 text-xl font-semibold">2. Changes to the ToS</h2>
-        <p className="mb-2">
-          <span className="font-semibold">2.1.</span> We reserve the right to
-          update, modify, or replace any part of the ToS at any time by posting
-          the updated ToS on the Website. Your continued use of the Website or
-          Service after the effective date of the updated ToS constitutes your
-          acceptance of the new terms.
-        </p>
-      </section>
+<p>By accessing this website we assume you accept these terms and conditions. Do not continue to use Insta Load if you do not agree to take all of the terms and conditions stated on this page.</p>
 
-      <section className="mb-2 text-sm">
-        <h2 className="mb-2 text-xl font-semibold">3. Prohibited Uses</h2>
-        <p className="mb-2">
-          <span className="font-semibold">3.1.</span> You may not use the
-          Website or Service for any illegal activities, including but not
-          limited to:
-        </p>
-        <ul className="mb-2 list-disc pl-6">
-          <li>
-            Distributing or accessing content that infringes on other&apos;s
-            intellectual property rights;
-          </li>
-          <li>
-            Transmitting or distributing material that promotes violence,
-            discrimination, or illegal activities;
-          </li>
-          <li>
-            Harassing, stalking, or intimidating other users of the Website or
-            Service.
-          </li>
-        </ul>
-        <p className="mb-2">
-          <span className="font-semibold">3.2.</span> You agree not to engage in
-          any activities that could harm or disable the Website or Service or
-          interfere with other user&apos;s use and enjoyment of the Website or
-          Service.
-        </p>
-      </section>
+<p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+<h2><strong>Cookies</strong></h2>
+<p>We employ the use of cookies. By accessing Insta Load, you agreed to use cookies in agreement with the Insta Load's Privacy Policy.</p>
 
-      <section className="mb-2 text-sm">
-        <h2 className="mb-2 text-xl font-semibold">
-          4. Intellectual Property Rights
-        </h2>
-        <p className="mb-2">
-          <span className="font-semibold">4.1.</span> All content, including but
-          not limited to text, images, videos, and software, on the Website is
-          the property of the Website owner or its licensors. You are granted a
-          limited license to access and view the content for your personal,
-          non-commercial use only.
-        </p>
-        <p className="mb-2">
-          <span className="font-semibold">4.2.</span> You may not modify, copy,
-          distribute, transmit, display, perform, reproduce, publish, license,
-          create derivative works from, transfer, or sell any content from the
-          Website without the express prior written permission of the Website
-          owner.
-        </p>
-      </section>
+<p>Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
+<h2><strong>License</strong></h2>
+<p>Unless otherwise stated, Insta Load and/or its licensors own the intellectual property rights for all material on Insta Load. All intellectual property rights are reserved. You may access this from Insta Load for your own personal use subjected to restrictions set in these terms and conditions.</p>
 
-      <section className="mb-2 text-sm">
-        <h2 className="mb-2 text-xl font-semibold">
-          5. Disclaimers and Limitations of Liability
-        </h2>
-        <p className="mb-2">
-          <span className="font-semibold">5.1.</span> The Website and Service
-          are provided on an &ldquo;as is&ldquo; and &ldquo;as available&ldquo;
-          basis without any warranties, express or implied, including but not
-          limited to warranties of merchantability, fitness for a particular
-          purpose, and non-infringement.
-        </p>
-        <p className="mb-2">
-          <span className="font-semibold">5.2.</span> We shall not be liable for
-          any direct, indirect, incidental, special, consequential, or punitive
-          damages arising out of or in connection with your use of the Website
-          or Service.
-        </p>
-      </section>
+<p><strong>You must not:</strong></p>
+<ul>
+ 	<li>Republish material from Insta Load</li>
+ 	<li>Sell, rent or sub-license material from Insta Load</li>
+ 	<li>Reproduce, duplicate or copy material from Insta Load</li>
+ 	<li>Redistribute content from Insta Load</li>
+</ul>
+<p>This Agreement shall begin on the date hereof. Our Terms and Conditions were created with the help of the <a rel="nofollow noopener noreferrer" href="https://www.termsandconditionsgenerator.com/">Free Terms and Conditions Generator</a>.</p>
 
-      <section className="mb-2 text-sm">
-        <h2 className="mb-2 text-xl font-semibold">6. Governing Law</h2>
-        <p className="mb-2">
-          <span className="font-semibold">6.1.</span> These ToS shall be
-          governed by and construed in accordance with the laws of{" "}
-          <span className="italic">Algeria</span>, without regard to its
-          conflict of law provisions.
-        </p>
-      </section>
+<p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Insta Load does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of Insta Load, its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, Insta Load shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.</p>
 
-      <section className="mb-2 text-sm">
-        <h2 className="mb-2 text-xl font-semibold">7. Contact Information</h2>
-        <p className="mb-2">
-          <span className="font-semibold">7.1.</span> If you have any questions
-          or concerns about this ToS, please contact the Website owner at{" "}
-          <span className="italic">riadh.azzoun@hotmail.com</span>.
-        </p>
-      </section>
+<p>Insta Load reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.</p>
 
-      <section className="mb-2 text-sm">
-        <h2 className="mb-2 text-xl font-semibold">8. Acceptance of ToS</h2>
-        <p>
-          By accessing or using the Website or Service, you acknowledge that you
-          have read, understood, and agree to be bound by the terms and
-          conditions of this ToS.
-        </p>
-      </section>
+<p><strong>You warrant and represent that:</strong></p>
+<ul>
+ 	<li>You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;</li>
+ 	<li>The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;</li>
+ 	<li>The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy</li>
+ 	<li>The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.</li>
+</ul>
+<p>You hereby grant Insta Load a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.</p>
+<h2><strong>Hyperlinking to our Content</strong></h2>
+<p>The following organizations may link to our Website without prior written approval:</p>
+<ul>
+ 	<li>Government agencies;</li>
+ 	<li>Search engines;</li>
+ 	<li>News organizations;</li>
+ 	<li>Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and</li>
+ 	<li>System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.</li>
+</ul>
+<p>These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party’s site.</p>
 
-      <section className="mb-2 text-sm">
-        <p className="text-red-500">
-          <span className="font-semibold">Disclaimer :</span> This is was auto
-          generated by AI
-        </p>
-      </section>
+<p>We may consider and approve other link requests from the following types of organizations:</p>
+<ul>
+ 	<li>commonly-known consumer and/or business information sources;</li>
+ 	<li>dot.com community sites;</li>
+ 	<li>associations or other groups representing charities;</li>
+ 	<li>online directory distributors;</li>
+ 	<li>internet portals;</li>
+ 	<li>accounting, law and consulting firms; and</li>
+ 	<li>educational institutions and trade associations.</li>
+</ul>
+<p>We will approve link requests from these organizations if we decide that: (a) the link would not make us look unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of Insta Load; and (d) the link is in the context of general resource information.</p>
+
+<p>These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party’s site.</p>
+
+<p>If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to Insta Load. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.</p>
+
+<p>Approved organizations may hyperlink to our Website as follows:</p>
+<ul>
+ 	<li>By use of our corporate name; or</li>
+ 	<li>By use of the uniform resource locator being linked to; or</li>
+ 	<li>By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party’s site.</li>
+</ul>
+<p>No use of Insta Load's logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
+<h2><strong>iFrames</strong></h2>
+<p>Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.</p>
+<h2><strong>Content Liability</strong></h2>
+<p>We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.</p>
+<h2><strong>Your Privacy</strong></h2>
+<p>Please read our <a href="/privacy-policy">Privacy Policy</a></p>
+<h2><strong>Reservation of Rights</strong></h2>
+<p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it’s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
+<h2><strong>Removal of links from our website</strong></h2>
+<p>If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.</p>
+
+<p>We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.</p>
+<h2><strong>Disclaimer</strong></h2>
+<p>To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:</p>
+<ul>
+ 	<li>limit or exclude our or your liability for death or personal injury;</li>
+ 	<li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
+ 	<li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
+ 	<li>exclude any of our or your liabilities that may not be excluded under applicable law.</li>
+</ul>
+<p>The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.</p>
+
+<p>As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</p>
+</section>
     </div>
   );
 };
