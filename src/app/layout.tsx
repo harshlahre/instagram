0a13ea2bc76cae736e895feb66111612e45e523a
harshlahre <harshlahre109@gmail.com>
2025-01-03 23:24:21 +0530

    ther

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import { DM_Sans as FontSans } from "next/font/google";
 import { GoogleAnalytics } from '@next/third-parties/google';
 import { Navbar, Footer } from "@/components/layout";
-import Script from "next/script";
+import { Viewport } from "next";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ReactQueryProvider } from "@/components/providers/react-query-provider";
 import { cn } from "@/lib/utils";
@@ -12,6 +12,10 @@ const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const viewport: Viewport = {
+  themeColor: '#4285f4',
+}
 
 export const metadata: Metadata = {
   title: "Instagram Video Downloader - iGram Saver",
@@ -142,12 +146,6 @@ export default function RootLayout({
           "overflow-x-hidden bg-background font-sans antialiased"
         )}
       >
-        <Script
-          async
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2008420195999107`}
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
         <section>
       {/* Add JSON-LD to your page */}
       <script
