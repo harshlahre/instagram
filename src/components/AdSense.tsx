768443b871d263ecc134aac4a4cc2ae6483bb310
Harsh Lahre <harshlahre109@gmail.com>
2025-02-28 10:20:07 +0530

    aesg

---
+++ src/components/AdSense.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { useEffect } from "react";
+
+const AdSense = () => {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error("AdSense script error:", e);
+    }
+  }, []);
+
+  return (
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2295806962223513"
+    crossOrigin="anonymous"></script>
+  );
+};
+
+export default AdSense;
